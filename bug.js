4a94ab19ae61a5b05a3ab6895795de9ff68a7c79
BugHunter-X <askzenai@gmail.com>
2025-01-27 10:08:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+```javascript
+// In a React 19 functional component
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
+```
